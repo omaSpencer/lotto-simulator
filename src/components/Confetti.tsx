08c5cd30651f0053df87c891505a370913cd0c13
Zoltan Busi <oma.spencer@gmail.com>
2025-05-21 18:14:18 +0200

    feat: UI update

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -13,9 +13,7 @@ export const Confetti = ({ onRestart }: { onRestart: () => void }) => {
 
       <div className='absolute inset-0 items-center justify-center pointer-events-auto bg-primary text-white rounded-lg max-w-2xl m-auto max-h-[300px] p-4 shadow-sm flex flex-col gap-4'>
         <h2 className='font-bold title-fSize text-center'>
-          You Win!
-          <br />
-          Congratulation!
+          Jackpot! 🎉
         </h2>
 
         <button className='text-primary bg-white rounded-sm px-4 py-2 min-h-10 min-w-[180px] font-bold' onClick={onRestart}>
